a48b0e92e2af3770d9de5ac0eba6fc2fea07a98e
Albert Avkhadeev <61252538+TatarinAlba@users.noreply.github.com>
2023-07-25 12:33:41 +0300

    NFT

---
+++ nft.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFTCHecker is ERC721URIStorage {
+   using Counters for Counters.Counter;
+   Counters.Counter public _tokenIds;
+   address private owner;
+   string[] tokenUris = ["https://ibb.co/zf6XHFz", "https://ibb.co/02LtD2Y", "https://ibb.co/y4KK4SC"];
+
+   constructor() public ERC721("LeadBotItem", "LBI") {owner = msg.sender; }
+
+   function buyNFT(uint256 id) public payable
+    {
+       require(msg.sender.balance >= msg.value, "You have no enough money");
+       _tokenIds.increment();
+       uint256 newItemId = _tokenIds.current();
+       _mint(msg.sender, newItemId);
+       _setTokenURI(newItemId, tokenUris[id - 1]);
+    }
+
+    function withdrawMoney() public {
+       require(msg.sender == owner, "You are not allowed to do this operation");
+       payable(msg.sender).transfer(address(this).balance);
+   }
+}
